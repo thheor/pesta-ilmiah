790b0d5e4158fd76716dd29c85a7a000e95a7114
thheor <prstylukman@gmail.com>
2026-06-13 21:33:56 +0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import "dotenv/config";
 
-const BASE_URL = process.env.BASE_URL || "/pesta-ilmiah/";
-console.log(BASE_URL);
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  base: BASE_URL.toString(),
+    base: command === "serve" ? "/" : "/pesta-ilmiah/",
+  };
 });
