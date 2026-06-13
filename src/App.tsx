920aa962b300005ccd7344f3f334b0af4cc3095e
thheor <prstylukman@gmail.com>
2026-06-13 18:01:23 +0700

    add heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <section id="center">
       <h1>Pesta Ilmiah</h1>
