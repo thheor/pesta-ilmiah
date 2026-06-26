a939ef12642661ff3e0ebfa19064f687ae418994
thheor <prstylukman@gmail.com>
2026-06-26 16:11:14 +0700

    Add some custom style

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,11 +1,16 @@
+import { Navbar } from "@/components/navbar";
+import { Footer } from "@/components/footer";
+
 interface BasicPageProps extends React.PropsWithChildren {
   className?: string;
 }
 
 export function MainLayout({ className, children }: BasicPageProps) {
   return (
-    <main className="grid grid-cols-12 font-sans">
-      <div className={`${className} col-span-10 col-start-2 `}>{children}</div>
-    </main>
+    <>
+      <Navbar></Navbar>
+      <main className="font-sans">{children}</main>
+      <Footer />
+    </>
   );
 }
